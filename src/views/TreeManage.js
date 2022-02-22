476ea0ee1c1afccdf4dda71eed7e49f8bc6d8b2a
HAEJIN <j__0308@naver.com>
2022-02-22 23:33:57 +0900

    7. 트리관리

--- src/views/TreeManage.js
+++ src/views/TreeManage.js
@@ -2,10 +2,15 @@ import "../style/treemanage.scss";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import main_icon12_hover from "../assets/main_icons/main_icon12_hover.png";
-import insert_info from "../assets/basicinformation/12_basicinformation_before.png";
-import modify_info from "../assets/basicinformation/12_basicinformation_after.png";
-import reset_info from "../assets/basicinformation/12_basicinformation_before.png";
+import main_icon7_hover from "../assets/main_icons/main_icon7_hover.png";
+
+import tree_manage_01 from "../assets/treemanage/07-01.PNG";
+import tree_manage_02 from "../assets/treemanage/07-02.PNG";
+import tree_manage_03 from "../assets/treemanage/07-03.PNG";
+import tree_manage_04 from "../assets/treemanage/07-04.PNG";
+import tree_manage_05 from "../assets/treemanage/07-05.PNG";
+import tree_manage_06 from "../assets/treemanage/07-06.PNG";
+
 /*
   하단 title, summary, imagename 작성하여 개수만 늘려주면 됨
   title     : 버튼이름
@@ -14,29 +19,47 @@ import reset_info from "../assets/basicinformation/12_basicinformation_before.pn
  */
 const InitButtonData = [
   {
-    title: "회원(학원) 정보 입력",
-    summary: "회원(학원)정보의 회원 정보를 입력할 수 있습니다.",
-    imagename: insert_info,
+    title: "카테고리 선택하기",
+    summary: "설명 쓰세욤",
+    imagename: tree_manage_02,
   },
   {
-    title: "회원(학원) 정보 수정",
-    summary: "회원(학원)정보의 회원 정보를 수정할 수 있습니다.",
-    imagename: modify_info,
+    title: "카테고리 이름 입력",
+    summary: "설명 쓰세욤",
+    imagename: tree_manage_03,
   },
   {
-    title: "초기화",
-    summary: "회원(학원)정보의 회원 정보를 초기화 수 있습니다.",
-    imagename: reset_info,
+    title: "등록하기",
+    summary: "설명 쓰세욤",
+    imagename: tree_manage_04,
+  },
+  {
+    title: "수정할 데이터 입력",
+    summary: "설명 쓰세욤",
+    imagename: tree_manage_05,
+  },
+  {
+    title: "수정하기",
+    summary: "설명 쓰세욤",
+    imagename: tree_manage_06,
+  },
+  {
+    title: "DB업로드",
+    summary: "설명 쓰세욤",
+    imagename: tree_manage_06,
+    alertmessage: "DB가 업로드되었습니다",
   },
 ];
+
 const TreeManage = () => {
-  const [mainImage, setMainImage] = useState(reset_info);
+  const [mainImage, setMainImage] = useState(tree_manage_01);
   const [mainSummary, setMainSummary] = useState("");
+
   return (
     <div className="treemanage">
       {/* aside left :: 해당 페이지 정보 이미지로 표기하는 부분 */}
       <aside className="left">
-        <img className="treemanage-image left" src={main_icon12_hover} />
+        <img className="treemanage-image left" src={main_icon7_hover} />
       </aside>
       {/* pagename-main :: 해당 페이지의 주 내용 부분이 담기는 부분 */}
       <div className="treemanage-main">
@@ -46,7 +69,7 @@ const TreeManage = () => {
       {/* aside right :: 해당 페이지의 Flow를 설명하는 부분 + 메인으로 돌아가는 버튼 존재 */}
       <aside className="right">
         <Link to="/main">
-          <img className="treemanage-image right" src={main_icon12_hover} />
+          <img className="treemanage-image right" src={main_icon7_hover} />
         </Link>
         <section className="buttons">
           아래 버튼을 클릭하면 가운데 실행화면을 볼 수 있습니다.
@@ -58,6 +81,7 @@ const TreeManage = () => {
                 onClick={() => {
                   setMainImage(item.imagename);
                   setMainSummary(item.summary);
+                  if (item.alertmessage != null) alert(item.alertmessage);
                 }}
               >
                 <div className="button-value">{item.title}</div>
