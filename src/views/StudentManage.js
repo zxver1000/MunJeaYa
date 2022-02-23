f06b88a553bd397a22577fc6c1552a28a0eb9380
HAEJIN <j__0308@naver.com>
2022-02-23 17:33:33 +0900

    6. 수강생관리

--- src/views/StudentManage.js
+++ src/views/StudentManage.js
@@ -1,11 +1,26 @@
-import "../style/studentmanage.scss";
+import "../style/coursemanage.scss";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import main_icon12_hover from "../assets/main_icons/main_icon12_hover.png";
-import insert_info from "../assets/basicinformation/12_basicinformation_before.png";
-import modify_info from "../assets/basicinformation/12_basicinformation_after.png";
-import reset_info from "../assets/basicinformation/12_basicinformation_before.png";
+import main_icon6_hover from "../assets/main_icons/main_icon6_hover.png";
+
+import student_manage_01 from "../assets/studentmanage/06-01.png";
+import student_manage_02 from "../assets/studentmanage/06-02.png";
+import student_manage_03 from "../assets/studentmanage/06-03.png";
+import student_manage_04 from "../assets/studentmanage/06-04.png";
+import student_manage_05 from "../assets/studentmanage/06-05.png";
+import student_manage_06 from "../assets/studentmanage/06-06.png";
+import student_manage_07 from "../assets/studentmanage/06-07.png";
+import student_manage_08 from "../assets/studentmanage/06-08.png";
+import student_manage_09 from "../assets/studentmanage/06-09.png";
+import student_manage_10 from "../assets/studentmanage/06-10.png";
+import student_manage_11 from "../assets/studentmanage/06-11.png";
+import student_manage_12 from "../assets/studentmanage/06-12.png";
+import student_manage_13 from "../assets/studentmanage/06-13.png";
+import student_manage_14 from "../assets/studentmanage/06-14.png";
+import student_manage_15 from "../assets/studentmanage/06-15.png";
+import student_manage_16 from "../assets/studentmanage/06-16.png";
+
 /*
   하단 title, summary, imagename 작성하여 개수만 늘려주면 됨
   title     : 버튼이름
@@ -14,39 +29,102 @@ import reset_info from "../assets/basicinformation/12_basicinformation_before.pn
  */
 const InitButtonData = [
   {
-    title: "회원(학원) 정보 입력",
-    summary: "회원(학원)정보의 회원 정보를 입력할 수 있습니다.",
-    imagename: insert_info,
+    title: "excel 일괄등록",
+    summary: "엑셀에 있는 학생들 정보를 저장할 수 있습니다.",
+    imagename: student_manage_02,
+    alertMessage: "1의 학생을 등록하였습니다",
   },
   {
-    title: "회원(학원) 정보 수정",
-    summary: "회원(학원)정보의 회원 정보를 수정할 수 있습니다.",
-    imagename: modify_info,
+    title: "신규 학생 등록",
+    summary: "설명쓰세욤",
+    imagename: student_manage_03,
   },
   {
-    title: "초기화",
-    summary: "회원(학원)정보의 회원 정보를 초기화 수 있습니다.",
-    imagename: reset_info,
+    title: "학생 정보 입력",
+    summary: "설명쓰세욤",
+    imagename: student_manage_04,
+  },
+  {
+    title: "학생 등록하기",
+    summary: "설명쓰세욤",
+    imagename: student_manage_05,
+  },
+  {
+    title: "학생 정보 수정",
+    summary: "설명쓰세욤",
+    imagename: student_manage_06,
+  },
+  {
+    title: "수정 데이터 입력",
+    summary: "설명쓰세욤",
+    imagename: student_manage_07,
+  },
+  {
+    title: "학생 정보 수정",
+    summary: "설명쓰세욤",
+    imagename: student_manage_08,
+  },
+  {
+    title: "신규 수강 등록",
+    summary: "설명쓰세욤",
+    imagename: student_manage_09,
+  },
+  {
+    title: "수강 반 입력",
+    summary: "설명쓰세욤",
+    imagename: student_manage_10,
+  },
+  {
+    title: "수강 등록하기",
+    summary: "설명쓰세욤",
+    imagename: student_manage_11,
+  },
+  {
+    title: "수강 등록 수정",
+    summary: "설명쓰세욤",
+    imagename: student_manage_12,
+  },
+  {
+    title: "수정할 수강 반 데이터 입력",
+    summary: "설명쓰세욤",
+    imagename: student_manage_13,
+  },
+  {
+    title: "수강 등록 수정",
+    summary: "설명쓰세욤",
+    imagename: student_manage_14,
+  },
+  {
+    title: "수강 등록 삭제",
+    summary: "설명쓰세욤",
+    imagename: student_manage_15,
+    alertMessage: "수강 반이 삭제되었습니다.",
+  },
+  {
+    title: "학생 정보 삭제",
+    summary: "설명쓰세욤",
+    imagename: student_manage_16,
+    alertMessage: "학생 정보가 삭제되었습니다.",
   },
 ];
-const StudentManage = () => {
-  const [mainImage, setMainImage] = useState(reset_info);
+const CourseManage = () => {
+  const [mainImage, setMainImage] = useState(student_manage_01);
   const [mainSummary, setMainSummary] = useState("");
   return (
-    <div className="studentmanage">
+    <div className="coursemanage">
       {/* aside left :: 해당 페이지 정보 이미지로 표기하는 부분 */}
       <aside className="left">
-        <img className="studentmanage-image left" src={main_icon12_hover} />
+        <img className="coursemanage-image left" src={main_icon6_hover} />
       </aside>
       {/* pagename-main :: 해당 페이지의 주 내용 부분이 담기는 부분 */}
-      <div className="studentmanage-main">
+      <div className="coursemanage-main">
         <img className="image" src={mainImage} />
         <article className="summary">{mainSummary}</article>
       </div>
       {/* aside right :: 해당 페이지의 Flow를 설명하는 부분 + 메인으로 돌아가는 버튼 존재 */}
       <aside className="right">
         <Link to="/main">
-          <img className="studentmanage-image right" src={main_icon12_hover} />
+          <img className="coursemanage-image right" src={main_icon6_hover} />
         </Link>
         <section className="buttons">
           아래 버튼을 클릭하면 가운데 실행화면을 볼 수 있습니다.
@@ -58,6 +136,7 @@ const StudentManage = () => {
                 onClick={() => {
                   setMainImage(item.imagename);
                   setMainSummary(item.summary);
+                  if (item.alertMessage != null) alert(item.alertMessage);
                 }}
               >
                 <div className="button-value">{item.title}</div>
@@ -70,4 +149,4 @@ const StudentManage = () => {
   );
 };
 
-export default StudentManage;
+export default CourseManage;
