be38349f9af159c35bb3985f43e51c02ddea6b84
xrl0603 <xrl0603@naver.com>
2022-02-24 13:59:14 +0900

    시험지 만들기 1번 완료. 문제 8개 출제는 비워놓음

--- src/views/CreatePaper.js
+++ src/views/CreatePaper.js
@@ -4,9 +4,28 @@ import { Link } from "react-router-dom";
 
 import main_icon12_hover from "../assets/main_icons/main_icon12_hover.png";
 import go_main from "../assets/goMain.png";
-import insert_info from "../assets/basicinformation/12_basicinformation_before.png";
-import modify_info from "../assets/basicinformation/12_basicinformation_after.png";
-import reset_info from "../assets/basicinformation/12_basicinformation_before.png";
+import main_image from "../assets/createpaper/01-01.png";
+import category_select from "../assets/createpaper/01-02.png";
+import data_set from "../assets/createpaper/01-03.png";
+import data_change from "../assets/createpaper/01-04.png";
+import data_set_complate from "../assets/createpaper/01-05.png";
+import test_name from "../assets/createpaper/01-06.png";
+import difficulty_level from "../assets/createpaper/01-07.png";
+import test_type from "../assets/createpaper/01-08.png";
+import test_select from "../assets/createpaper/01-09.png";
+
+import new_main_image from "../assets/createpaper/01-10.png";
+import quest_2 from "../assets/createpaper/01-11.png";
+import quest_4 from "../assets/createpaper/01-12.png";
+import quest_6 from "../assets/createpaper/01-13.png";
+
+import quest_easy from "../assets/createpaper/01-15.png";
+import quest_difficult from "../assets/createpaper/01-16.png";
+import quest_type from "../assets/createpaper/01-17.png";
+import quest_random from "../assets/createpaper/01-18.png";
+import quest_short_answer from "../assets/createpaper/01-19.png";
+import quest_select_answer from "../assets/createpaper/01-20.png";
+
 /*
   하단 title, summary, imagename 작성하여 개수만 늘려주면 됨
   title     : 버튼이름
@@ -15,41 +34,109 @@ import reset_info from "../assets/basicinformation/12_basicinformation_before.pn
  */
 const InitButtonDataFirst = [
   {
-    title: "회원(학원) 정보 입력",
-    summary: "회원(학원)정보의 회원 정보를 입력할 수 있습니다.",
-    imagename: insert_info,
+    title: "카테고리 선택",
+    summary: "입력",
+    imagename: category_select,
   },
   {
-    title: "회원(학원) 정보 수정",
-    summary: "회원(학원)정보의 회원 정보를 수정할 수 있습니다.",
-    imagename: modify_info,
+    title: "SET",
+    summary: "입력",
+    imagename: data_set,
   },
   {
-    title: "초기화",
-    summary: "회원(학원)정보의 회원 정보를 초기화 수 있습니다.",
-    imagename: reset_info,
+    title: "데이터 변경",
+    summary: "입력",
+    imagename: data_change,
+  },
+  {
+    title: "시험지 만들기 설정",
+    summary: "입력",
+    imagename: data_set_complate,
+    message: "시험지 설정이 완료되었습니다.",
+  },
+  {
+    title: "시험지명 입력",
+    summary: "입력",
+    imagename: test_name,
+  },
+  {
+    title: "난이도별 출제",
+    summary: "입력",
+    imagename: difficulty_level,
+  },
+  {
+    title: "유형별 출제",
+    summary: "입력",
+    imagename: test_type,
+  },
+  {
+    title: "문제 선택 출제",
+    summary: "입력",
+    imagename: test_select,
   },
 ];
 const InitButtonDataSecond = [
   {
-    title: "회원(학원) 정보 입력",
-    summary: "회원(학원)정보의 회원 정보를 입력할 수 있습니다.",
-    imagename: insert_info,
+    title: "2문제 씩 출제",
+    summary: "입력",
+    imagename: quest_2,
   },
   {
-    title: "회원(학원) 정보 수정",
-    summary: "회원(학원)정보의 회원 정보를 수정할 수 있습니다.",
-    imagename: modify_info,
+    title: "4문제 씩 출제",
+    summary: "입력",
+    imagename: quest_4,
   },
   {
-    title: "초기화",
-    summary: "회원(학원)정보의 회원 정보를 초기화 수 있습니다.",
-    imagename: reset_info,
+    title: "6문제 씩 출제",
+    summary: "입력",
+    imagename: quest_6,
+  },
+  {
+    title: "8문제 씩 출제",
+    summary: "입력",
+    imagename: "",
+  },
+  {
+    title: "쉬운 순 문제 정렬",
+    summary: "입력",
+    imagename: quest_easy,
+  },
+  {
+    title: "어려운 순 문제 정렬",
+    summary: "입력",
+    imagename: quest_difficult,
+  },
+  {
+    title: "유형 순 문제 정렬",
+    summary: "입력",
+    imagename: quest_type,
+  },
+  {
+    title: "랜덤 문제 정렬",
+    summary: "입력",
+    imagename: quest_random,
+  },
+
+  {
+    title: "주관식 우선 출제",
+    summary: "입력",
+    imagename: quest_short_answer,
+  },
+  {
+    title: "객관식 우선 출제",
+    summary: "입력",
+    imagename: quest_select_answer,
+  },
+  {
+    title: "랜덤 문제 정렬",
+    summary: "입력",
+    imagename: new_main_image,
+    message: "시험지가 저장, 학생들에게 배포되었습니다.",
   },
 ];
 const CreatePaper = () => {
   const [buttonType, setButtonType] = useState(false);
-  const [mainImage, setMainImage] = useState(reset_info);
+  const [mainImage, setMainImage] = useState(main_image);
   const [mainSummary, setMainSummary] = useState("");
   return (
     <div className="createpaper">
@@ -58,10 +145,22 @@ const CreatePaper = () => {
         <img className="createpaper-image left" src={main_icon12_hover} />
         <section className="buttons">
           아래 버튼을 클릭하여 시험지 만드는 방법을 알 수 있습니다.
-          <button className="button" onClick={() => setButtonType(false)}>
+          <button
+            className="button"
+            onClick={() => {
+              setButtonType(false);
+              setMainImage(main_image);
+            }}
+          >
             <div className="button-value">(1) 시험지 만들기 보기</div>
           </button>
-          <button className="button" onClick={() => setButtonType(true)}>
+          <button
+            className="button"
+            onClick={() => {
+              setButtonType(true);
+              setMainImage(new_main_image);
+            }}
+          >
             <div className="button-value">(2) 시험지 설정 보기</div>
           </button>
         </section>
@@ -87,6 +186,7 @@ const CreatePaper = () => {
                     onClick={() => {
                       setMainImage(item.imagename);
                       setMainSummary(item.summary);
+                      if (item.message != null) alert(item.message);
                     }}
                   >
                     <div className="button-value">{item.title}</div>
@@ -101,6 +201,7 @@ const CreatePaper = () => {
                     onClick={() => {
                       setMainImage(item.imagename);
                       setMainSummary(item.summary);
+                      if (item.message != null) alert(item.message);
                     }}
                   >
                     <div className="button-value">{item.title}</div>
