c07668d5479c9e8f6b5de0936378ce1d672252a5
HAEJIN <j__0308@naver.com>
2022-02-25 14:24:37 +0900

    2. 나의 시험지

--- src/views/MyPapers.js
+++ src/views/MyPapers.js
@@ -2,11 +2,26 @@ import "../style/mypapers.scss";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import main_icon12_hover from "../assets/main_icons/main_icon12_hover.png";
+import main_icon2_hover from "../assets/main_icons/main_icon2_hover.png";
 import go_main from "../assets/goMain.png";
-import insert_info from "../assets/basicinformation/12_basicinformation_before.png";
-import modify_info from "../assets/basicinformation/12_basicinformation_after.png";
-import reset_info from "../assets/basicinformation/12_basicinformation_before.png";
+
+import my_papers_01 from "../assets/mypapers/02-01.png";
+import my_papers_02 from "../assets/mypapers/02-02.png";
+import my_papers_03 from "../assets/mypapers/02-03.png";
+import my_papers_04 from "../assets/mypapers/02-04.png";
+import my_papers_05 from "../assets/mypapers/02-05.png";
+import my_papers_06 from "../assets/mypapers/02-06.png";
+import my_papers_07 from "../assets/mypapers/02-07.png";
+import my_papers_08 from "../assets/mypapers/02-08.png";
+import my_papers_09 from "../assets/mypapers/02-09.png";
+import my_papers_10 from "../assets/mypapers/02-10.png";
+import my_papers_11 from "../assets/mypapers/02-11.png";
+import my_papers_12 from "../assets/mypapers/02-12.png";
+import my_papers_13 from "../assets/mypapers/02-13.png";
+import my_papers_14 from "../assets/mypapers/02-14.png";
+import my_papers_15 from "../assets/mypapers/02-15.png";
+import my_papers_16 from "../assets/mypapers/02-16.png";
+
 /*
   하단 title, summary, imagename 작성하여 개수만 늘려주면 됨
   title     : 버튼이름
@@ -15,29 +30,103 @@ import reset_info from "../assets/basicinformation/12_basicinformation_before.pn
  */
 const InitButtonData = [
   {
-    title: "회원(학원) 정보 입력",
-    summary: "회원(학원)정보의 회원 정보를 입력할 수 있습니다.",
-    imagename: insert_info,
+    title: "새로운 시험지 만들기",
+    summary: "설명쓰세욤",
+    imagename: my_papers_01,
+    alertMessage: "메인페이지에서 [시험지만들기]를 누르면 확인하실 수 있습니다",
+  },
+  ,
+  {
+    title1: "오래된 순서로 정렬",
+    title2: "최근 순서로 정렬",
+    summary: "설명쓰세욤",
+    imagename1: my_papers_02,
+    imagename2: my_papers_01,
+    flag: true,
   },
   {
-    title: "회원(학원) 정보 수정",
-    summary: "회원(학원)정보의 회원 정보를 수정할 수 있습니다.",
-    imagename: modify_info,
+    title: "시험지 보기",
+    summary: "설명쓰세욤",
+    imagename: my_papers_03,
   },
   {
-    title: "초기화",
-    summary: "회원(학원)정보의 회원 정보를 초기화 수 있습니다.",
-    imagename: reset_info,
+    title: "검색할 반과 시험지 입력",
+    summary: "설명쓰세욤",
+    imagename: my_papers_04,
+  },
+  {
+    title: "검색하기",
+    summary: "설명쓰세욤",
+    imagename: my_papers_05,
+  },
+  {
+    title: "시험지 선택",
+    summary: "설명쓰세욤",
+    imagename: my_papers_06,
+  },
+  {
+    title: "시험지 배포",
+    summary: "설명쓰세욤",
+    imagename: my_papers_07,
+  },
+  {
+    title: "시험지 배포할 학생 입력",
+    summary: "설명쓰세욤",
+    imagename: my_papers_08,
+  },
+  {
+    title: "선택된 학생에게 시험지 배포",
+    summary: "설명쓰세욤",
+    imagename: my_papers_09,
+    alertMessage: "해당 학생에게 배포되었습니다.",
+  },
+  {
+    title: "빠른 정답",
+    summary: "설명쓰세욤",
+    imagename: my_papers_10,
+  },
+  {
+    title: "정답 + 해설",
+    summary: "설명쓰세욤",
+    imagename: my_papers_11,
+  },
+  {
+    title: "문제만 출력",
+    summary: "설명쓰세욤",
+    imagename: my_papers_12,
+  },
+  {
+    title: "전체 출력",
+    summary: "설명쓰세욤",
+    imagename: my_papers_13,
+  },
+  {
+    title: "인쇄하기",
+    summary: "설명쓰세욤",
+    imagename: my_papers_14,
+    alertMessage: "인쇄되었습니다.",
+  },
+  {
+    title: "삭제할 시험지 선택",
+    summary: "설명쓰세욤",
+    imagename: my_papers_15,
+  },
+  {
+    title: "시험지 삭제",
+    summary: "설명쓰세욤",
+    imagename: my_papers_16,
+    alertMessage: "시험지가 삭제되었습니다.",
   },
 ];
+
 const MyPapers = () => {
-  const [mainImage, setMainImage] = useState(reset_info);
+  const [mainImage, setMainImage] = useState(my_papers_01);
   const [mainSummary, setMainSummary] = useState("");
   return (
     <div className="mypapers">
       {/* aside left :: 해당 페이지 정보 이미지로 표기하는 부분 */}
       <aside className="left">
-        <img className="mypapers-image left" src={main_icon12_hover} />
+        <img className="mypapers-image left" src={main_icon2_hover} />
       </aside>
       {/* pagename-main :: 해당 페이지의 주 내용 부분이 담기는 부분 */}
       <div className="mypapers-main description">
@@ -57,11 +146,25 @@ const MyPapers = () => {
                 className="button"
                 key={"button" + index}
                 onClick={() => {
-                  setMainImage(item.imagename);
-                  setMainSummary(item.summary);
+                  if (item.flag == null) {
+                    setMainImage(item.imagename);
+                    setMainSummary(item.summary);
+                    if (item.alertMessage != null) alert(item.alertMessage);
+                  } else {
+                    item.flag
+                      ? setMainImage(item.imagename1)
+                      : setMainImage(item.imagename2);
+                    item.flag = !item.flag;
+                  }
                 }}
               >
-                <div className="button-value">{item.title}</div>
+                {item.flag == null ? (
+                  <div className="button-value">{item.title}</div>
+                ) : item.flag ? (
+                  <div className="button-value">{item.title1}</div>
+                ) : (
+                  <div className="button-value">{item.title2}</div>
+                )}
               </button>
             );
           })}
